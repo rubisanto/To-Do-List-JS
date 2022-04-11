d429f674fc0c2e59fe3f447eb23d13d4592c3582
Antoine Gogce <antoinegogceapb@hotmail.fr>
2022-04-11 13:47:05 +0200

    #2 - deleteCheck

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const todoList = document.querySelector(".todo-list");
 
 //Event Listeners
 todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteCheck);
 
 // Functions
 function addTodo(event) {
@@ -32,4 +33,19 @@ function addTodo(event) {
   todoList.appendChild(todoDiv);
   //   CLEAR TODO INPUT value
   todoInput.value = "";
+}
+
+function deleteCheck(e) {
+  const item = e.target;
+  // DELETE THE TODO
+  if (item.classList[0] === "trash-btn") {
+    const todo = item.parentElement;
+    todo.remove();
+  }
+
+  //   CHECK MARK
+  if (item.classList[0] === "complete-btn") {
+    const todo = item.parentElement;
+    todo.classList.toggle("completed");
+  }
 }
