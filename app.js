d440f80820eb03b2d274fbbec6a08f65cc8c7961
Antoine Gogce <antoinegogceapb@hotmail.fr>
2022-04-11 11:32:39 +0200

    #1 - Liste ajoutable

---
+++ app.js
@@ -0,0 +1,35 @@
+//Selectors
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+//Event Listeners
+todoButton.addEventListener("click", addTodo);
+
+// Functions
+function addTodo(event) {
+  // PREVENT FORM FROM SUBMITTING
+  event.preventDefault();
+  // Todo DIV
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+  //   CREATE LI
+  const newTodo = document.createElement("li");
+  newTodo.innerText = todoInput.value;
+  newTodo.classList.add("todo-item");
+  todoDiv.appendChild(newTodo);
+  // CHECK MARK BUTTON
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = '<i class="fas fa-check"></i>';
+  completedButton.classList.add("complete-btn");
+  todoDiv.appendChild(completedButton);
+  // CHECK TRASH BUTTON
+  const trashButton = document.createElement("button");
+  trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+  trashButton.classList.add("trash-btn");
+  todoDiv.appendChild(trashButton);
+  //   APPEND TO THE LIST
+  todoList.appendChild(todoDiv);
+  //   CLEAR TODO INPUT value
+  todoInput.value = "";
+}
